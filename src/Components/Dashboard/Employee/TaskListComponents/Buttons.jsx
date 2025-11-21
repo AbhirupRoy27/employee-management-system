eaaf1fa1baecbef195273ae49dbb57eb82c07f14
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-11-21 19:56:16 +0530

    style: minor style change to the button.

--- src/Components/Dashboard/Employee/TaskListComponents/Buttons.jsx
+++ src/Components/Dashboard/Employee/TaskListComponents/Buttons.jsx
@@ -13,6 +13,7 @@ function Buttons({ status }) {
           className="active:cursor-progress text-sm backdrop-blur cursor-pointer font-poppins uppercase bg-blue-800/90 hover:bg-blue-900 hover:shadow-lg shadow-black/20 text-white active:scale-95 transform transition-all duration-150 py-2 px-4 rounded-md font-bold lg:w-[140px] lg:h-14"
           onClick={(e) => {
             e.stopPropagation()
+            // console.log(status.task_id)
             setCurrentStatus('accepted')
           }}
         >
@@ -27,6 +28,7 @@ function Buttons({ status }) {
           className="active:cursor-progress text-sm cursor-pointer text-gray-50 font-poppins uppercase bg-green-600 hover:bg-green-700 hover:shadow-lg shadow-black/20 active:scale-95 transform transition-all duration-150 py-2 px-4 font-bold rounded-md lg:w-[140px] lg:h-14"
           onClick={(e) => {
             e.stopPropagation()
+            // console.log(status.task_id)
             setCurrentStatus('completed')
           }}
         >
@@ -38,7 +40,7 @@ function Buttons({ status }) {
     return (
       <div className="flex h-full justify-center items-center">
         <button
-          className="text-sm cursor-pointer text-gray-950  font-poppins uppercase bg-gray-500/30  py-2 px-4 rounded-md lg:w-[140px] font-bold lg:h-14"
+          className="text-sm cursor-pointer text-gray-950 font-poppins uppercase bg-gray-500/30  py-2 px-4 rounded-md lg:w-[140px] font-bold lg:h-14"
           disabled
         >
           {'Completed'}
