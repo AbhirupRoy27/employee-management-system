c3b23f1c454b3ddd7bab5d4149d8e021aedc87c0
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-11-22 20:35:17 +0530

    feat(data): insted of getting statis data from json now the context has data from db. using the connection from backend.

--- src/Context/taskContext.jsx
+++ src/Context/taskContext.jsx
@@ -1,23 +1,43 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, useContext, useState } from 'react'
-import task from '../DB/employee.json'
+import { createContext, useContext, useEffect, useState } from 'react'
+// import task from '../DB/employee.json'
+import axios from 'axios'
 
 const TaskContext = createContext()
 
 export default function TaskProvider({ children }) {
-  const [taskDetails, setTaskDetails] = useState({})
-  const [tasks, setTask] = useState(task[0].tasks)
-  const [filterTask, setFilterTask] = useState(tasks)
+  const [tasks, setTask] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      try {
+        const response = await axios.post(
+          'http://localhost:3000/api/employee/get-tasks',
+          {
+            email: 'arjub33141wt@gmail.com',
+          }
+        )
+        setTask((prev) => [...prev, ...response.data.tasks])
+      } catch (err) {
+        if (err.response) {
+          if (err.response.status === 404) {
+            console.log('Not found:', err.response.data.message)
+            return
+          }
+          console.log('API error:', err.response.data)
+        } else {
+          console.log('Network failure:', err)
+        }
+      }
+    }
+    getTasks()
+  }, [])
 
   return (
     <TaskContext.Provider
       value={{
         tasks,
         setTask,
-        setTaskDetails,
-        taskDetails,
-        filterTask,
-        setFilterTask,
       }}
     >
       {children}
