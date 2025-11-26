998b43383fdf1cd22337b6c11898707b901334f5
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-11-26 11:47:13 +0530

    fix: API endpoint fixed.

--- src/Utils/Admin/handleAdminForm.js
+++ src/Utils/Admin/handleAdminForm.js
@@ -70,8 +70,8 @@ export const handleInput = (e, setAdminFormData) => {
 
 const addTask = async (adminFormData) => {
   const { data } = await axios.post(
-    // 'https://ems-backend-iota-wine.vercel.app/api/admin/add-task',
-    'http://localhost:3000/api/admin/add-task',
+    'https://ems-backend-iota-wine.vercel.app/api/admin/add-task',
+    // 'http://localhost:3000/api/admin/add-task',
     adminFormData
   )
   return data
