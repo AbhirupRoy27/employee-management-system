60944bd92b47474496ff97a15674a24b7574a6d3
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-12-11 19:48:15 +0530

    made the navigation as a differnet component.

---
+++ src/Components/Dashboard/Admin/navBar/AdminNav.jsx
@@ -0,0 +1,49 @@
+import { LayoutDashboard, Menu, X } from 'lucide-react'
+import { useState } from 'react'
+
+function AdminNav({ setActiveTab }) {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <div className="min-w-[200px] sm:w-1/10 bg-white/10 sm:min-h-screen">
+      <div className="flex justify-between px-2 items-center">
+        <header className="flex justify-center items-center py-3 px-2 gap-3">
+          <LayoutDashboard size={40} color="gray" />
+          <b className="text-xl tracking-wider">Admin Dashboard</b>
+        </header>
+        <section className="flex flex-col items-center sm:mt-10 sm:hidden">
+          <button onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? <X size={40} /> : <Menu size={30} />}
+          </button>
+        </section>
+      </div>
+      <section
+        className={`${
+          isOpen ? 'flex absolute w-full bg-black/90 h-screen' : 'hidden'
+        } sm:flex flex-col`}
+      >
+        <div className="flex flex-col sm:mt-10">
+          <button
+            className="py-4 bg-black hover:bg-white/35 transition-all duration-300 ease border-b border-white/20 uppercase tracking-wider"
+            onClick={() => {
+              setActiveTab('AddEmpTask')
+              setIsOpen(false)
+            }}
+          >
+            Add Employee
+          </button>
+          <button
+            className="py-4 bg-black hover:bg-white/35 transition-all duration-300 ease border-b border-white/20 uppercase tracking-wider"
+            onClick={() => {
+              setActiveTab('AddEmpForm')
+              setIsOpen(false)
+            }}
+          >
+            Add Task
+          </button>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default AdminNav
--- src/Page/Dashboard/AdminDashboard.jsx
+++ src/Page/Dashboard/AdminDashboard.jsx
@@ -3,36 +3,20 @@ import { useState } from 'react'
 import Logout from '../Logout/Logout'
 import AddEmpForm from '../../Components/Dashboard/Admin/addEmployee/AddEmpForm'
 import AdminForm from '../../Components/Dashboard/Admin/addTask/AdminForm'
-import { LayoutDashboard } from 'lucide-react'
+import AdminNav from '../../Components/Dashboard/Admin/navBar/AdminNav'
 
 function AdminDashboard() {
   const [isActive, setIsActive] = useState(false)
+  const [activeTab, setActiveTab] = useState('AddEmpTask')
 
   return (
     <div className="bg-[#1C1C1C] min-h-screen min-w-[354px]">
       {isActive && <Logout setIsActive={setIsActive} />}
       <div className="flex flex-col sm:flex-row text-white ">
-        <div className="min-w-[200px] sm:w-1/10 bg-white/10 min-h-screen">
-          <header className="flex justify-center items-center py-3 px-2 gap-3">
-            <LayoutDashboard size={60} color="gray" />
-            <b className="text-xl tracking-wider">Admin Dashboard</b>
-          </header>
-          <section>
-            <div className="flex flex-col mt-10">
-              <button className="py-2 bg-white/20 hover:bg-white/35 transition-all duration-300 ease border-b border-black uppercase tracking-wider">
-                Add Employee
-              </button>
-              <button className="py-2 bg-white/20 hover:bg-white/35 transition-all duration-300 ease border-b border-black uppercase tracking-wider">
-                Add Task
-              </button>
-            </div>
-          </section>
-        </div>
+        <AdminNav setActiveTab={setActiveTab} />
         <div className="w-full">
           <Header setIsActive={setIsActive} />
-          <AdminForm />
-
-          <AddEmpForm />
+          {activeTab == 'AddEmpTask' ? <AdminForm /> : <AddEmpForm />}
         </div>
       </div>
     </div>
