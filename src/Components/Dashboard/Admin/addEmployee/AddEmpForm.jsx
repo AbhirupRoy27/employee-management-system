e0502b4943dbd489927764d610f16937a13da37f
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-12-12 11:52:44 +0530

    refactor: simplify AddEmpForm by removing CloseButton and using Suspense for EmpDetails

--- src/Components/Dashboard/Admin/addEmployee/AddEmpForm.jsx
+++ src/Components/Dashboard/Admin/addEmployee/AddEmpForm.jsx
@@ -1,18 +1,13 @@
-import React, { useState } from 'react'
-import CloseButton from './Components/CloseButton'
-import EmpDetails from './Components/EmpDetails'
+import React, { lazy, Suspense } from 'react'
+import DefaultLoader from '../../../Loader/DefaultLoader'
+const EmpDetails = lazy(() => import('./Components/EmpDetails'))
 
 function AddEmpForm() {
-  const [isOpen, setIsOpen] = useState(false)
   return (
     <div className="mx-4 sm:mx-15  py-3 text-gray-50 mt-5">
-      <button
-        className="bg-gray-50/10 hover:bg-gray-50/15 py-2 px-4 rounded border border-gray-50/40 transition-colors duration-300 ease-in-out cursor-pointer "
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        {isOpen ? <CloseButton /> : 'Add New Employeee'}
-      </button>
-      {isOpen && <EmpDetails />}
+      <Suspense fallback={<DefaultLoader />}>
+        <EmpDetails />
+      </Suspense>
     </div>
   )
 }
