726b65830d026ebc531f4069dc5bdeac7f1525be
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-12-12 11:58:23 +0530

    feat: enhance DefaultLoader with styled loading indicator

--- src/Components/Loader/DefaultLoader.jsx
+++ src/Components/Loader/DefaultLoader.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Loader } from 'lucide-react'
 
 function DefaultLoader() {
   return (
-    <div>
-      <h1>Loading..</h1>
+    <div className="flex justify-center items-center h-[75vh] bg-gray-50/10">
+      <h1 className="flex gap-2 items-center text-3xl text-gray-50/40 tracking-widest font-mono font-semibold">
+        <Loader size={40} />
+        Loading...
+      </h1>
     </div>
   )
 }
