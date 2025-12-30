cd48fbce440434bcbf97c1eb1e6359e0cbbc3901
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-12-30 13:53:56 +0530

    feat: made a dedicated loader for the App.

--- src/Components/Dashboard/Employee/TaskView/TaskView.jsx
+++ src/Components/Dashboard/Employee/TaskView/TaskView.jsx
@@ -12,14 +12,4 @@ function TaskView() {
   )
 }
 
-export const Loader = () => {
-  return (
-    <div className="mx-4 sm:mx-15 flex flex-col lg:flex-row ">
-      <div className="text-3xl flex min-h-[60vh] w-full justify-center items-center text-amber-50 tracking-widest">
-        Loading...
-      </div>
-    </div>
-  )
-}
-
 export default TaskView
---
+++ src/Shared/Pages/LoadingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function LoadingPage() {
+  return (
+    <div className="mx-4 sm:mx-15 flex flex-col lg:flex-row ">
+      <div className="text-3xl flex min-h-[60vh] w-full justify-center items-center text-amber-50 tracking-widest">
+        Loading...
+      </div>
+    </div>
+  )
+}
+
+export default LoadingPage
