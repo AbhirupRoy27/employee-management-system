8e1bbe9483751cdafeb3d211ab82485793a45fca
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-12-30 13:52:59 +0530

    clean the app.jsx

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,60 +1,14 @@
-import { lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
-import Login from '../Page/Auth/Login'
-import EmpDashboard from '../Page/Dashboard/EmpDashboard'
-import AdminDashboard from '../Page/Dashboard/AdminDashboard'
-import NotFound from '../Page/NotFound/NotFound'
-import ProtectedRoute from '../Components/Auth/ProtectedRoute/ProtectedRoute'
-import AdminProtectedRoute from '../Components/Auth/ProtectedRoute/AdminProtectedRoute'
-import TaskList from '../Components/Dashboard/Employee/TaskList'
-import { Loader } from '../Components/Dashboard/Employee/TaskView/TaskView'
-import TaskInfo from '../Components/Dashboard/Employee/TaskInfo'
-
-const TaskView = lazy(() =>
-  import('../Components/Dashboard/Employee/TaskView/TaskView')
-)
+import PublicRoutes from './Routes/PublicRoutes'
+import AdminRoutes from './Routes/AdminRoutes'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route
-          path="/Employee-dashboard"
-          element={
-            // <ProtectedRoute>
-            <EmpDashboard />
-            // </ProtectedRoute>
-          }
-        >
-          <Route
-            index
-            element={
-              <>
-                <TaskInfo />
-                <TaskList />
-              </>
-            }
-          />
-          <Route
-            path="task-details"
-            element={
-              <Suspense fallback={<Loader />}>
-                <TaskView />
-              </Suspense>
-            }
-          />
-        </Route>
-        <Route
-          path="/admin-dashboard"
-          element={
-            // <AdminProtectedRoute>
-            <AdminDashboard />
-            // </AdminProtectedRoute>
-          }
-        />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/*" element={<PublicRoutes />} />
+        <Route path="/admin/*" element={<AdminRoutes />} />
       </Routes>
     </>
   )
