b54ae4abaaa479218ca805524b463f9aa71c4d05
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-12-30 12:22:29 +0530

    refactor: moved the app and main to a folder and fixed index.html

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,18 +1,18 @@
 import { lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
-import Login from './Page/Auth/Login'
-import EmpDashboard from './Page/Dashboard/EmpDashboard'
-import AdminDashboard from './Page/Dashboard/AdminDashboard'
-import NotFound from './Page/NotFound/NotFound'
-import ProtectedRoute from './Components/Auth/ProtectedRoute/ProtectedRoute'
-import AdminProtectedRoute from './Components/Auth/ProtectedRoute/AdminProtectedRoute'
-import TaskList from './Components/Dashboard/Employee/TaskList'
-import { Loader } from './Components/Dashboard/Employee/TaskView/TaskView'
-import TaskInfo from './Components/Dashboard/Employee/TaskInfo'
+import Login from '../Page/Auth/Login'
+import EmpDashboard from '../Page/Dashboard/EmpDashboard'
+import AdminDashboard from '../Page/Dashboard/AdminDashboard'
+import NotFound from '../Page/NotFound/NotFound'
+import ProtectedRoute from '../Components/Auth/ProtectedRoute/ProtectedRoute'
+import AdminProtectedRoute from '../Components/Auth/ProtectedRoute/AdminProtectedRoute'
+import TaskList from '../Components/Dashboard/Employee/TaskList'
+import { Loader } from '../Components/Dashboard/Employee/TaskView/TaskView'
+import TaskInfo from '../Components/Dashboard/Employee/TaskInfo'
 
 const TaskView = lazy(() =>
-  import('./Components/Dashboard/Employee/TaskView/TaskView')
+  import('../Components/Dashboard/Employee/TaskView/TaskView')
 )
 
 function App() {
--- src/App/main.jsx
+++ src/App/main.jsx
@@ -1,11 +1,11 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import '../index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import UsercontextProvider from './Context/Usercontext.jsx'
-import TaskProvider from './Context/taskContext.jsx'
-import FilterTaskContext from './Context/filterTaskContext.jsx'
-import TaskCountProvider from './Context/taskCountContext.jsx'
+import UsercontextProvider from '../Context/Usercontext.jsx'
+import TaskProvider from '../Context/taskContext.jsx'
+import FilterTaskContext from '../Context/filterTaskContext.jsx'
+import TaskCountProvider from '../Context/taskCountContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <>
